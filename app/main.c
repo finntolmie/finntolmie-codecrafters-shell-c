03c8976414c556ae17355af5d1ede00fe13c7a1b
finntolmie <finn.tolmie12@gmail.com>
2024-12-28 17:15:20 +1100

    6. type

--- app/main.c
+++ app/main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+const char *builtins[] = {"echo", "exit", NULL};
+
 void command_exit(char *arguments) {
   if (arguments == NULL || arguments[0] == '\0')
     exit(0);
@@ -16,6 +18,19 @@ void command_echo(char *arguments) {
   printf("%s", arguments);
 }
 
+void command_type(char *arguments) {
+  if (arguments == NULL || arguments[0] == '\0')
+    return;
+  // check builtins first
+  for (const char **cmd = builtins; *cmd != NULL; cmd++) {
+    if (strcmp(*cmd, arguments) == 0) {
+      printf("%s is a shell builtin", *cmd);
+      return;
+    }
+  }
+  printf("%s: not found", arguments);
+}
+
 int main() {
   char input[100];
   while (1) {
@@ -27,6 +42,8 @@ int main() {
       command_exit(input + sizeof("exit"));
     else if (strncmp(input, "echo", sizeof("echo") - 1) == 0)
       command_echo(input + sizeof("echo"));
+    else if (strncmp(input, "type", sizeof("type") - 1) == 0)
+      command_type(input + sizeof("type"));
     else
       printf("%s: command not found", input);
     printf("\n");
