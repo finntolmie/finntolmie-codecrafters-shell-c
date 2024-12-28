46886f0469907a0fec300ebc2068edfd442df26c
finntolmie <finn.tolmie12@gmail.com>
2024-12-28 16:45:49 +1100

    3. refactor

--- app/main.c
+++ app/main.c
@@ -2,14 +2,10 @@
 #include <string.h>
 
 int main() {
-  // Flush after every printf
-  setbuf(stdout, NULL);
-
-  // Wait for user input
   char input[100];
   while (1) {
-    memset(input, 0, sizeof(input));
     printf("$ ");
+    fflush(stdout);
     fgets(input, 100, stdin);
     input[strlen(input) - 1] = '\0';
     printf("%s: command not found\n", input);
