16053279a91b7485ae988fcde3ed5ba8afc16dd6
finntolmie <finn.tolmie12@gmail.com>
2024-12-28 17:00:08 +1100

    4. exit

--- app/main.c
+++ app/main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+void command_exit(char *arguments) {
+  if (arguments == NULL || arguments[0] == '\0')
+    exit(0);
+
+  int exit_code = atoi(arguments);
+  exit(exit_code);
+}
 
 int main() {
   char input[100];
@@ -8,6 +17,9 @@ int main() {
     fflush(stdout);
     fgets(input, 100, stdin);
     input[strlen(input) - 1] = '\0';
+    if (strncmp(input, "exit", 4) == 0) {
+      command_exit(input + sizeof("exit"));
+    }
     printf("%s: command not found\n", input);
   }
   return 0;
