8bb7e583244cea7c7d99d9954702e3ba90d1ece2
finntolmie <finn.tolmie12@gmail.com>
2024-12-28 16:43:13 +1100

    3. REPL

--- app/main.c
+++ app/main.c
@@ -5,13 +5,14 @@ int main() {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // Uncomment this block to pass the first stage
-  printf("$ ");
-
   // Wait for user input
   char input[100];
-  fgets(input, 100, stdin);
-  input[strlen(input) - 1] = '\0';
-  printf("%s: command not found\n", input);
+  while (1) {
+    memset(input, 0, sizeof(input));
+    printf("$ ");
+    fgets(input, 100, stdin);
+    input[strlen(input) - 1] = '\0';
+    printf("%s: command not found\n", input);
+  }
   return 0;
 }
